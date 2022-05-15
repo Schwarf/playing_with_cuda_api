940cff923fe6cfc24011f401eea44a4abb721b34
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-15 19:23:56 +0200

    Add dynamic case.

--- shared_memory.cu
+++ shared_memory.cu
@@ -22,11 +22,20 @@ int main()
 	tracker.copy_device_array_to_host_array(output_static, device_input, n);
 	for(int i=0; i < n; ++i)
 	{
-		if(output_static[i]>0 && output_static[i] != expected_output[i])
+		if(output_static[i] ==0 || output_static[i] != expected_output[i])
 			std::cerr <<"ERROR: " << output_static[i] << " vs " << expected_output[i] << "\n";
 	}
 
-
+	// dynamic cass
+	auto device_input2 = tracker.allocate_device_memory(n, "d_input2");
+	tracker.copy_host_array_to_device_array(input, device_input2, n);
+	reverse_array_dynamic_shared_memory<int><<<1, n, n*sizeof(int)>>>(device_input2, n);
+	tracker.copy_device_array_to_host_array(output_dynamic, device_input2, n);
+	for(int i=0; i < n; ++i)
+	{
+		if(output_dynamic[i] ==0 || output_dynamic[i] != expected_output[i])
+			std::cerr <<"ERROR: " << output_dynamic[i] << " vs " << expected_output[i] << "\n";
+	}
 
 	return 0;
 }
