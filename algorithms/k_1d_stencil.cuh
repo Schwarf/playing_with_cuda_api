288b18a5efde6089c12cdc9063a47e589ee0e4ce
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-30 15:17:18 +0200

    Add random number generation.

--- algorithms/k_1d_stencil.cuh
+++ algorithms/k_1d_stencil.cuh
@@ -6,6 +6,43 @@
 #define K_1D_STENCIL_CUH
 #include <iostream>
 
+template <typename T>
+__global__ void
+kth_stencil(T * input, T * output, int input_size, int k)
+{
+	extern __shared__ T shared_memory[];
+	// id of thread in block
+	int local_thread_id = threadIdx.x;
+
+	// first index of output-element by the block
+	int start_of_thread_block = blockIdx.x * blockDim.y;
+
+	// The id of the thread in the scope of the grid
+	int globalId = local_thread_id + start_of_thread_block;
+
+	if(globalId > input_size)
+		return;
+
+	// Fetching into shared memory
+	shared_memory[local_thread_id] = input[globalId];
+	if(local_thread_id < 2 && blockDim.x + globalId < input_size)
+		shared_memory[blockDim.x + local_thread_id] = input[blockDim.x + globalId];
+
+	__syncthreads();
+
+	T denom= static_cast<T>(2*k+1);
+	if(globalId < input_size - 2*k)
+	{
+		output[globalId] = shared_memory[local_thread_id];
+		for(int i = 1 ; i < (2*k+1); ++i)
+		{
+			output[globalId] = shared_memory[local_thread_id + i];
+		}
+		output[globalId] /= denom;
+	}
+
+}
+
 template<typename T>
 T *kth_stencil(T *input, int input_size, int k)
 {
@@ -27,6 +64,8 @@ T *kth_stencil(T *input, int input_size, int k)
 
 	return stencil;
 }
+
+
 
 
 #endif //K_1D_STENCIL_CUH
--- stencils.cu
+++ stencils.cu
@@ -2,16 +2,49 @@
 // Created by andreas on 30.05.22.
 //
 #include "algorithms/k_1d_stencil.cuh"
+#include "helper/memory_tracker.cuh"
 #include <iostream>
+#include <type_traits>
+#include <random>
+
+template <typename T, typename std::enable_if<std::is_integral<T>::value, bool>::type = true>
+static inline T get_random_integral(const T & lower_bound, const T & upper_bound, T * output, int output_size)
+{
+	static std::mt19937 generator;
+	auto int_distribution = std::uniform_int_distribution<T>(lower_bound, upper_bound);
+	for(int i = 0; i < output_size; i++)
+	{
+		output[i] = int_distribution(generator);
+	}
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, bool>::type = true>
+static inline T get_random_float(const T & lower_bound, const T & upper_bound, T * output, int output_size)
+{
+	static std::mt19937 generator;
+	auto real_distribution = std::uniform_real_distribution<T>(lower_bound, upper_bound);
+	for(int i = 0; i < output_size; i++)
+	{
+		output[i] = real_distribution(generator);
+	}
+}
+
+
+
 int main()
 {
-	constexpr int size = 8;
-	constexpr int k = 1;
-	int A[size] = {0,1,2,3,4,5,6,7};
+	constexpr int size = 800;
+	constexpr int k = 2;
+	float A[size];
+	get_random_float<float>(0.f, 100.f, A, size);
 	auto B = kth_stencil(A, size, k);
 	for(int i =0; i < size-2*k; ++i)
 	{
-		std::cout << B[i] << std::endl;
+		std::cout << (A[i] + A[i+1] + A[i+2] + A[i+3] + A[i+4])/5.f << "  " << B[i] << std::endl;
+		//std::cout << (A[i] + A[i+1] + A[i+2] )/3 / B[i] << std::endl;
 	}
+	//auto tracker = MemoryTracker<int>();
+	//tracker.allocate_device_memory()
+
 
 }
