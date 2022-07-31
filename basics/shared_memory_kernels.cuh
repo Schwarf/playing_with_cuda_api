8a8dc8b75556beca31721cf45065b7f2f2b4c701
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-31 20:06:05 +0200

    Old WIP

--- basics/shared_memory_kernels.cuh
+++ basics/shared_memory_kernels.cuh
@@ -4,12 +4,15 @@
 
 #ifndef SHARED_MEMORY_KERNELS_CUH
 #define SHARED_MEMORY_KERNELS_CUH
+#include <curand.h>
 
 template<typename T, size_t memory_size>
 __global__ void reverse_array_static_shared_memory(T * input_array, int size)
 {
 	__shared__ T shared[memory_size];
-	auto thread_id = threadIdx.x;
+	const auto thread_id = threadIdx.x;
+	if( thread_id > size)
+		return;
 	auto reversed_thread_id = size - thread_id -1;
 	shared[thread_id] = input_array[thread_id];
 	__syncthreads();
@@ -21,7 +24,7 @@ template<typename T>
 __global__ void reverse_array_dynamic_shared_memory(T * input_array, int size)
 {
 	extern __shared__ T shared[];
-	auto thread_id = threadIdx.x;
+	const auto thread_id = threadIdx.x;
 	auto reversed_thread_id = size - thread_id -1;
 	shared[thread_id] = input_array[thread_id];
 	__syncthreads();
--- shared_memory.cu
+++ shared_memory.cu
@@ -9,7 +9,8 @@
 
 int main()
 {
-	constexpr int n = 200;
+	constexpr int n = 1024;
+	const int threads_per_block = 1024;
 	int input[n], expected_output[n], output_static[n], output_dynamic[n];
 	for (int i = 0; i < n; ++i) {
 		input[i] = 1 + i;
@@ -18,17 +19,18 @@ int main()
 	auto tracker = MemoryTracker<int>();
 	auto device_input = tracker.allocate_device_memory(n, "d_input");
 	tracker.copy_host_array_to_device_array(input, device_input, n);
-	reverse_array_static_shared_memory<int, n><<<1, n>>>(device_input, n);
+	const int blocks = (n + threads_per_block - 1)/threads_per_block;
+	reverse_array_static_shared_memory<int, n><<<1, threads_per_block>>>(device_input, n);
 	tracker.copy_device_array_to_host_array(output_static, device_input, n);
 	for (int i = 0; i < n; ++i) {
 		if (output_static[i] == 0 || output_static[i] != expected_output[i])
 			std::cerr << "ERROR: " << output_static[i] << " vs " << expected_output[i] << "\n";
 	}
 
-	// dynamic cass
+	// dynamic case
 	auto device_input2 = tracker.allocate_device_memory(n, "d_input2");
 	tracker.copy_host_array_to_device_array(input, device_input2, n);
-	reverse_array_dynamic_shared_memory<int><<<1, n, n * sizeof(int)>>>(device_input2, n);
+	reverse_array_dynamic_shared_memory<int><<<1, threads_per_block, n * sizeof(int)>>>(device_input2, n);
 	tracker.copy_device_array_to_host_array(output_dynamic, device_input2, n);
 	for (int i = 0; i < n; ++i) {
 		if (output_dynamic[i] == 0 || output_dynamic[i] != expected_output[i])
