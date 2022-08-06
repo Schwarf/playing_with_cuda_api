e3c7f119903406850ffcb49a9c9ce18ce58f63cb
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-06 18:06:30 +0200

    Add helper macros.

--- random.cu
+++ random.cu
@@ -5,6 +5,13 @@
 #include "curand.h"
 #include <stdexcept>
 #include "helper/memory_tracker.cuh"
+#define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
+      printf("Error at %s:%d\n",__FILE__,__LINE__);     \
+      return EXIT_FAILURE;}} while(0)
+#define CURAND_CALL(x) do { if((x) != CURAND_STATUS_SUCCESS) { \
+      printf("Error at %s:%d\n",__FILE__,__LINE__);            \
+      return EXIT_FAILURE;}} while(0)
+
 int main()
 {
 	curandGenerator_t generator;
